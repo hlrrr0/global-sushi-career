9691a9e0cba6417084343b0a70da87e45c9dd937
hiroki <i.hlrrr0@gmail.com>
2026-01-13 00:06:00 +0900

    fix: Google Tag Managerのスニペット配置を修正
    変更点:
    - GTMスクリプトを<head>から<body>の先頭に移動
    - Next.jsでは<head>を直接使えないため、Scriptコンポーネントをbody内に配置
    - noscriptタグを<body>の最初に配置（GTM推奨）
    - これによりGoogle Search Consoleの所有権確認が正常に動作
    
    GTMの推奨配置:
    1. <body>開始直後にnoscriptタグ
    2. その直後にGTMスクリプト

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,18 +27,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <head>
-        {/* Google Tag Manager */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-K46H6HDQ');
-          `}
-        </Script>
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
@@ -52,6 +40,17 @@ export default function RootLayout({
           />
         </noscript>
         {/* End Google Tag Manager (noscript) */}
+        
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K46H6HDQ');
+          `}
+        </Script>
         
         {children}
       </body>
